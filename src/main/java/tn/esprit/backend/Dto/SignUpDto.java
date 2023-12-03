b3e35b7fe73504bee37f6917a6e54fb3a7534211
oubeidMezni <oubeid.mezni@esprit.tn>
2023-12-03 15:49:01 +0100

    user

---
+++ src/main/java/tn/esprit/backend/Dto/SignUpDto.java
@@ -0,0 +1,6 @@
+package tn.esprit.backend.Dto;
+
+public record SignUpDto (String firstName,String lastName, String login, char[] password){
+
+
+}
---
+++ src/main/java/tn/esprit/backend/entities/User.java
@@ -0,0 +1,29 @@
+package tn.esprit.backend.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+    @Column(name = "login", nullable = false)
+    private String login;
+    @Column(name = "Password", nullable = false)
+    private String password;
+}
